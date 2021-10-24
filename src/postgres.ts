70943d9c16ddcf7a207ef0a9938747e88b87f867
Yasunori Koga <yasunori.koga@linecorp.com>
2021-10-24 18:35:07 +0900

    fix: model name

--- src/postgres.ts
+++ src/postgres.ts
@@ -1,18 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const models = require('../models');
+import { getTaskResult } from './types';
 
 export const getTask = (id: number) => {
-    return models.tasks.findOne({
+    console.log(models);
+    console.log(models.Tasks);
+    console.log(models.tasks);
+    return models.Tasks.findOne({
         where: {
             id: id
         }
-    }) as Promise<any>;
+    }) as Promise<getTaskResult>;
 };
 
 export const getTasksForUser = (userId: string) => {
     return models.tasks.findAll({
         where: {
-        user_id: userId
+            user_id: userId
         }
-    }) as Promise<any>;
+    }) as Promise<Array<getTaskResult>>;
 };
