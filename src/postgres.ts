2010db3ceda11d8d7e39088f106c984aa17a1e13
Yasunori Koga <yasunori.koga@linecorp.com>
2021-10-24 18:19:42 +0900

    fix: import

--- src/postgres.ts
+++ src/postgres.ts
@@ -1,10 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const models = require('../../models');
+const models = require('../models');
 
 export const getTask = (id: number) => {
     return models.tasks.findOne({
-      where: {
-        id: id
-      }
+        where: {
+            id: id
+        }
     }) as Promise<any>;
-  };
+};
+
+export const getTasksForUser = (userId: string) => {
+    return models.tasks.findAll({
+        where: {
+        user_id: userId
+        }
+    }) as Promise<any>;
+};
