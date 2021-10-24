07c55b81fae44573d9ad6da6bccae0a802951599
Yasunori Koga <yasunori.koga@linecorp.com>
2021-10-24 16:30:25 +0900

    test: change log

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.command('/todo_add', async ({command, ack, say, client}) => {
     return;
   };
 
-  await say(command.text);
+  console.log(command.text);
 
   const textArray = command.text.split(' ');
 
