f4a672c12cfb1fc352006e6d9d607907f7ff60b4
motikoma <motikoma5125@gmail.com>
2021-10-24 16:00:16 +0900

    /todo_add 作成途中

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,38 @@ const app = new App({
   console.log('⚡️ Bolt app is running!');
 })();
 
+app.command('/todo_add', async ({command, ack, say}) => {
+  await ack();
+  // DBに人名とタスク名を追加する
+
+  if(!command.text){
+    await say(`タスクを記載してください`);
+    return;
+  };
+
+  const textArray = command.text.split(' ');
+
+  const user: string[] = [];
+  const task: string[] = [];
+
+  textArray.forEach(text => {
+    if(/^@.*/.test(text)) {
+      user.push(text);
+    } else {
+      task.push(text);
+    }
+});
+
+  // 第一引数に@がなければ自分のタスクとして追加する
+  if (user.length === 0) {
+    user.push(command.user_id);
+  };
+
+  // 第一引数に@があれば、別の人のタスクとして追加する
+
+  await say(`〜さんのタスクを追加しました`);
+})
+
 app.command('/todo_ls', async ({command, ack, say}) => {
   await ack();
   await say(`${command.text}`);
