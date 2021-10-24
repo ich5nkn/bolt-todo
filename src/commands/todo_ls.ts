893d7457d75e88e1e6ecc1957f7cd71ace341996
Yasunori Koga <yasunori.koga@linecorp.com>
2021-10-24 21:11:18 +0900

    fix: show user name

--- src/commands/todo_ls.ts
+++ src/commands/todo_ls.ts
@@ -7,24 +7,26 @@ export const todo_ls = (app: App) => {
 
         console.log(command.text);
 
-        const sayTask = async (userId: string) => {
+        const sayTask = async (userId: string, userName: string) => {
             const tasks = await getTasksForUser(userId);
             if(tasks){
-                await say(tasks.map(task => `- ${task.dataValues.task_name}`).join('\n'));
+                const taskList = tasks.map(task => `・${task.dataValues.task_name}`).join('\n')
+                await say(`${userName} のTODO\n${taskList}`);
             } else {
                 await say("タスクがありません");
             }
         };
 
         if(!command.text) {
-            await sayTask(command.user_id);
+            await sayTask(command.user_id, '自分');
             return;
         }
 
         if(/<@.*>$/.test(command.text)) {
             const result = command.text.match(/(?<=\<@).*?(?=\|)/);
-            if(result) {
-                await sayTask(result[0]);
+            const userName = command.text.match(/(?<=\|).*?(?=\>)/);
+            if(result && userName) {
+                await sayTask(result[0], userName[0]);
                 return;
             }
         }
