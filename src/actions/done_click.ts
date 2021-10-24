a8d046839a9de2bbaa1664412b62e6b58446d0a2
Yasunori Koga <yasunori.koga@linecorp.com>
2021-10-24 22:01:35 +0900

    test: console payload

--- src/actions/done_click.ts
+++ src/actions/done_click.ts
@@ -1,8 +1,9 @@
 import { App } from '@slack/bolt';
 
 export const done_click = (app: App) => {
-    app.action('done_click', async({ ack, say }) => {
+    app.action('done_click', async({ ack, say, payload }) => {
         await ack();
+        console.log(payload);
         await say('ボタンが押された');
     })
 }
