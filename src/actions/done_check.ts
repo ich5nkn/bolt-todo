de5ebef161c9c3dccb372bf3a1f58ab6765998ae
motikoma <motikoma5125@gmail.com>
2021-10-30 22:36:49 +0900

    respond動作確認

---
+++ src/actions/done_check.ts
@@ -0,0 +1,15 @@
+import { App } from '@slack/bolt';
+import {updateDoneTask} from "../postgres";
+
+export const done_check = (app: App) => {
+    app.action('done_check', async({ ack, respond, payload }) => {
+        await ack();
+        const anyPayload = payload as any;
+        console.log(anyPayload);
+        console.log(anyPayload.value);
+        if(Number(anyPayload.value)){
+            updateDoneTask(Number(anyPayload.value));
+        }
+        await respond("タスクを完了しました")
+    })
+}
